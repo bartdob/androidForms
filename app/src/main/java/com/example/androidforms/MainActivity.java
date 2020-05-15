902a707462264dfe5d0289fb85608390ba8e4be7
Bartosz Dobrosielski <dobry1@pm.me>
2020-05-15 14:35:43 +0200

    add logistics/ variables

--- app/src/main/java/com/example/androidforms/MainActivity.java
+++ app/src/main/java/com/example/androidforms/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.androidforms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView backArrow, settings, voice, cart;
+    private EditText nameEdtTxt, emailEdtTxt, descriptionEdtTxt, websiteEdtTxt;
+    private Spinner spinner;
+    private RadioGroup radioGroup;
+    private Button btnFinish;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initWidget();
+    }
+
+    private void initWidget(){
+        backArrow = (ImageView) findViewById(R.id.backArrow);
+        settings = (ImageView) findViewById(R.id.settings);
+        voice = (ImageView) findViewById(R.id.voice);
+        cart = (ImageView) findViewById(R.id.cart);
+
+        nameEdtTxt = (EditText) findViewById(R.id.nameEdtTxt);
+        emailEdtTxt = (EditText) findViewById(R.id.emailEdtTxt);
+        descriptionEdtTxt = (EditText) findViewById(R.id.descriptionEdtText);
+        websiteEdtTxt = (EditText) findViewById(R.id.websiteEdtTxt);
+
+        spinner = (Spinner) findViewById(R.id.countrySpinner);
+
+        radioGroup = (RadioGroup) findViewById(R.id.genderRadioGroup);
+        btnFinish = (Button) findViewById(R.id.btnFinish);
+
+
     }
 }
