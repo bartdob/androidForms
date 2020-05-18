633d531364dce1b528ec28ddb72691c039bcf12f
Bartosz Dobrosielski <dobry1@pm.me>
2020-05-18 13:26:37 +0200

    add logistics for buutons

--- app/src/main/java/com/example/androidforms/MainActivity.java
+++ app/src/main/java/com/example/androidforms/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.androidforms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private Button btnFinish;
 
+    private String country = "";
+    private ArrayList<String> countries;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initWidget();
+        countries = new ArrayList<>();
+        countries.add("USA");
+        countries.add("Canada");
+        countries.add("UK");
+        countries.add("Rusia");
+        countries.add("Portugies");
+        countries.add("Spain");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, countries);
+        spinner.setAdapter(adapter);
+
+        initOnClickListeners ();
+
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishRegistering();
+            }
+        });
     }
 
     private void initWidget(){
@@ -42,7 +71,57 @@ public class MainActivity extends AppCompatActivity {
 
         radioGroup = (RadioGroup) findViewById(R.id.genderRadioGroup);
         btnFinish = (Button) findViewById(R.id.btnFinish);
+    }
 
+    private void finishRegistering(){
+        String name = nameEdtTxt.getText().toString();
+        String email = emailEdtTxt.getText().toString();
+        String describtion = descriptionEdtTxt.getText().toString();
+        String website = websiteEdtTxt.getText().toString();
 
+        String gender = "";
+        int checkedRb = radioGroup.getCheckedRadioButtonId();
+        switch (checkedRb){
+            case R.id.rbMale:
+                gender = "Male";
+                break;
+            case R.id.rbFemale:
+                gender = "Female";
+                break;
+            case R.id.rbOther:
+                gender = "Ohter";
+                break;
+            default:
+                break;
+
+        }
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                country =  countries.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                country = "No Country Selected";
+            }
+        });
+    }
+    private void initOnClickListeners (){
+            backArrow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "back arrow tapped", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            settings.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "settings tapped", Toast.LENGTH_LONG).show();
+                }
+            });
     }
 }
+
